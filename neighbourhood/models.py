4ab173eb80b24aabfcf1fd7dfb016d0800c38e75
barbaramootian <barbara.mootian@student.moringaschool.com>
2022-03-21 16:33:50 +0300

    created models for neighborhood and profile

--- neighbourhood/models.py
+++ neighbourhood/models.py
@@ -1,47 +1,47 @@
-# from django.db import models
-# from django.contrib.auth.models import User 
-# from cloudinary.models import CloudinaryField
-# import datetime as dt
+from django.db import models
+from django.contrib.auth.models import User 
+from cloudinary.models import CloudinaryField
+import datetime as dt
 
-# # Create your models here.
-# class Neighborhood(models.Model):
-#     hood_name = models.CharField(max_length=100)
-#     hood_location = models.CharField(max_length=100)
-#     occupants = models.IntegerField(default=0)
+# Create your models here.
+class Neighborhood(models.Model):
+    hood_name = models.CharField(max_length=100)
+    hood_location = models.CharField(max_length=100)
+    occupants = models.IntegerField(default=0)
 
-#     def __str__(self):
-#             return self.hood_name
+    def __str__(self):
+            return self.hood_name
 
-#     def save_hood(self):
-#         self.save()
+    def save_hood(self):
+        self.save()
 
-#     def delete_hood(self):
-#         self.delete()
+    def delete_hood(self):
+        self.delete()
 
-#     @classmethod
-#     def search_neighbourhood(cls,hood_id):
-#         hood = cls.objects.get(id=hood_id)
-#         return hood 
+    @classmethod
+    def search_neighbourhood(cls,hood_id):
+        hood = cls.objects.get(id=hood_id)
+        return hood 
 
-#     def update_hood(self,name):
-#         self.hood_name = name
-#         self.save()
+    def update_hood(self,name):
+        self.hood_name = name
+        self.save()
 
 
-# class Profile(models.Model):
-#     profile_picture = CloudinaryField('image', null=True)
-#     user_location = models.CharField(max_length=200)
-#     prof_user = models.ForeignKey(User,on_delete=models.CASCADE,null=True)
-#     hood_id = models.ForeignKey(Neighborhood,on_delete=models.CASCADE,null=True)
+class Profile(models.Model):
+    profile_picture = CloudinaryField('image', null=True)
+    user_location = models.CharField(max_length=200)
+    prof_user = models.ForeignKey(User,on_delete=models.CASCADE,null=True)
+    hood_id = models.ForeignKey(Neighborhood,on_delete=models.CASCADE,null=True)
 
-#     def __str__(self):
-#         return self.prof_user.username
+    def __str__(self):
+        return self.prof_user.username
 
-#     def save_profile(self):
-#         self.save()
+    def save_profile(self):
+        self.save()
 
-#     def delete_profile(self):
-#         self.delete()
+    def delete_profile(self):
+        self.delete()
 
 # class Business(models.Model):
 #     business_name = models.CharField(max_length=200)

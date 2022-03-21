14b9842c32976c3ff660334c3f36fceeea9a80a4
barbaramootian <barbara.mootian@student.moringaschool.com>
2022-03-21 16:32:35 +0300

    added class for business and profile

--- neighbourhood/apps.py
+++ neighbourhood/apps.py
@@ -1,6 +1,6 @@
-# from django.apps import AppConfig
+from django.apps import AppConfig
 
 
-# class NeighbourhoodConfig(AppConfig):
-#     default_auto_field = 'django.db.models.BigAutoField'
-#     name = 'neighbourhood'
+class NeighbourhoodConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'neighbourhood'
--- neighbourhood/forms.py
+++ neighbourhood/forms.py
@@ -1,17 +1,17 @@
-# from django import forms
-# from .models import *
+from django import forms
+from .models import *
 
-# class BusinessForm(forms.ModelForm):
-#     class Meta:
-#         model = Business
-#         exclude = ['business_owner']
+class BusinessForm(forms.ModelForm):
+    class Meta:
+        model = Business
+        exclude = ['business_owner']
 
-# class ProfileForm(forms.ModelForm):
-#     class Meta:
-#         model = Profile
-#         exclude = ['prof_user']
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        exclude = ['prof_user']
 
-# class PostForm(forms.ModelForm):
-#     class Meta:
-#         model = Post
-#         exclude = ['post_owner']
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        exclude = ['post_owner']

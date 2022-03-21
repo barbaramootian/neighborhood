756da43a43b10279aa1c23bee8e6387a260aad3a
barbaramootian <barbara.mootian@student.moringaschool.com>
2022-03-21 16:34:23 +0300

    added new models

--- neighbourhood/models.py
+++ neighbourhood/models.py
@@ -43,71 +43,71 @@ class Profile(models.Model):
     def delete_profile(self):
         self.delete()
 
-# class Business(models.Model):
-#     business_name = models.CharField(max_length=200)
-#     business_email = models.EmailField()
-#     business_owner = models.ForeignKey(User,on_delete=models.CASCADE)
-#     business_hood_id = models.ForeignKey(Neighborhood,on_delete=models.CASCADE,null=True)
+class Business(models.Model):
+    business_name = models.CharField(max_length=200)
+    business_email = models.EmailField()
+    business_owner = models.ForeignKey(User,on_delete=models.CASCADE)
+    business_hood_id = models.ForeignKey(Neighborhood,on_delete=models.CASCADE,null=True)
 
-#     class Meta:
-#         ordering = ['-id']
+    class Meta:
+        ordering = ['-id']
 
-#     def __str__(self):
-#         return self.business_name
+    def __str__(self):
+        return self.business_name
 
-#     def save_business(self):
-#         self.save()
+    def save_business(self):
+        self.save()
 
-#     def delete_business(self):
-#         self.delete()
+    def delete_business(self):
+        self.delete()
     
-#     @classmethod
-#     def search_business_by_title(cls,search_term):
-#         post = cls.objects.filter(business_name__icontains=search_term)
-#         return post    
-#     @classmethod
-#     def search_business(cls,biz_id):
-#         business = cls.objects.get(id=biz_id)
-#         return business 
+    @classmethod
+    def search_business_by_title(cls,search_term):
+        post = cls.objects.filter(business_name__icontains=search_term)
+        return post    
+    @classmethod
+    def search_business(cls,biz_id):
+        business = cls.objects.get(id=biz_id)
+        return business 
     
 
-# class Post(models.Model):
-#     post_picture = CloudinaryField('image', null=True)
-#     post_name = models.CharField(max_length=200)
-#     post_description = models.TextField()
-#     date_posted = models.DateField(auto_now=True)
-#     post_owner = models.ForeignKey(User,on_delete=models.CASCADE,null=True)
-#     hood_post = models.ForeignKey(Neighborhood,on_delete=models.CASCADE,null=True)
-#     def __str__(self):
-#         return self.post_name
+class Post(models.Model):
+    post_picture = CloudinaryField('image', null=True)
+    post_name = models.CharField(max_length=200)
+    post_description = models.TextField()
+    date_posted = models.DateField(auto_now=True)
+    post_owner = models.ForeignKey(User,on_delete=models.CASCADE,null=True)
+    hood_post = models.ForeignKey(Neighborhood,on_delete=models.CASCADE,null=True)
+    def __str__(self):
+        return self.post_name
 
-#     class Meta:
-#         ordering = ['-id']
+    class Meta:
+        ordering = ['-id']
 
-#     def save_post(self):
-#         self.save()
+    def save_post(self):
+        self.save()
 
-#     def delete_post(self):
-#         self.delete()
+    def delete_post(self):
+        self.delete()
 
-#     @classmethod
-#     def get_posts(cls):
-#         posts = cls.objects.all()
-#         return posts
+    @classmethod
+    def get_posts(cls):
+        posts = cls.objects.all()
+        return posts
 
-# class ContactInfo(models.Model):
-#     health_department = models.CharField(max_length=200)
-#     police_department = models.CharField(max_length=200)
-#     hood = models.ForeignKey(Neighborhood,on_delete=models.CASCADE,null=True)
+class ContactInfo(models.Model):
+    health_department = models.CharField(max_length=200)
+    police_department = models.CharField(max_length=200)
+    hood = models.ForeignKey(Neighborhood,on_delete=models.CASCADE,null=True)
 
-#     def __str__(self):
-#         return self.health_department
+    def __str__(self):
+        return self.health_department
 
-#     def save_contact(self):
-#         self.save()
+    def save_contact(self):
+        self.save()
 
-#     def delete_contact(self):
-#         self.delete()
+    def delete_contact(self):
+        self.delete()
 
 
 
